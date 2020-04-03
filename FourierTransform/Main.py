75d8d344ad833b2e44cbdda3c1553d4bc9e8f362
Nicholas Miller <nicholascmiller123@gmail.com>
2020-04-03 16:09:27 -0400

    Created vector module to handle angle calculations

--- FourierTransform/Main.py
+++ FourierTransform/Main.py
@@ -1,40 +1,36 @@
 import tkinter as tk
-import math
+from modules.Vector import Vector
 
+root = tk.Tk()
 canvas = tk.Canvas(width=200, height=200)
 canvas.pack()
 
 center = 100, 100
+millis_per_frame = int(1000 / 30)
+time = 0
+
+label = tk.Label(root, text="Time: " + str(time))
+label.pack()
 
 xy = [(center[0], center[1] - 3), (center[0], center[1] + 3), (center[0] + 50, center[1] + 3), (center[0] + 50, center[1] + 6), (center[0] + 60, center[1]), (center[0] + 50, center[1] - 6), (center[0] + 50, center[1] - 3)]
 polygon_item = canvas.create_polygon(xy)
 
-def getangle(event):
-    dx = canvas.canvasx(event.x) - center[0]
-    dy = canvas.canvasy(event.y) - center[1]
-    try:
-        return complex(dx, dy) / abs(complex(dx, dy))
-    except ZeroDivisionError:
-        return 0.0
-
-def press(event):
-    global start
-    start = getangle(event)
-
-def motion(event):
-    global start
-    angle = getangle(event) / start
+def update_time():
+    global time
+    time = time + millis_per_frame
     offset = complex(center[0], center[1])
-    newxy = []
+    new_xy = []
+
+    v = Vector(0.1, complex(1, 0))
 
     for x, y in xy:
-        v = angle * (complex(x,y) - offset) + offset
-        newxy.append(v.real)
-        newxy.append(v.imag)
+        angle = v.calc_angle(time) * (complex(x,y) - offset) + offset
+        new_xy.append(angle.real)
+        new_xy.append(angle.imag)
 
-    canvas.coords(polygon_item, *newxy)
+    canvas.coords(polygon_item, *new_xy)
+    root.after(millis_per_frame, update_time)
 
-canvas.bind("<Button-1>", press)
-canvas.bind("<B1-Motion>", motion)
+update_time()
 
-tk.mainloop()
+root.mainloop()
